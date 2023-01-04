858d0a2135d904d6346ff3575c6dcd3310532761
Youssef <ettolbayos@gmail.com>
2023-01-04 20:45:21 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,27 @@ variable "aws_api_token" {
    description = "AWS API Token"
    default = "FwoGZXIvYXdzELz//////////wEaDM5bav9qNr5uks7wryK1AVhv7VBXgjadlKbTAowKdNFWU4GbA1A0pZyZjN9Nx/YY3PriyENHurqu1aycu85mOC+1w9jqhE7y21LE+XKh+3fNWzOKOHtOxie4VoNfFku3N35yHanfzOeqVfO1kQZaSxsH19OfejE3BEHhO3/3JkqHZ5pRdNLa+rtXSVt4mLQH1ZA9MCaer1I1AHygOFAUD8lxY9q68LkfGlZORx2rdD+ZYNq8mh6j4Je2vgCSX4u7/v/1OPAo15HXnQYyLed74rY/XwuF+E3UpAhq7jXif1jZ3VuYLSBRaYz/Y9EmJSeUN3wyLLgPoYmE8A=="
 }
+
+variable "num_workers" {
+  type        = number
+  description = "Number of worker nodes."
+  default     = 3
+}
+
+variable "pod_network_cidr" {
+  type        = string
+  description = "IP address range of the Pod network of the cluster in CIDR format."
+  default     = "10.244.0.0/16"
+}
+
+variable "public_key" {
+  type        = string
+  description = "Path to a file with a public key in OpenSSH format. The public key must belong to var.private_key."
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "private_key" {
+  type        = string
+  description = "Path to a file with a private key. The private key must belong to var.public_key."
+  default     = "~/.ssh/id_rsa"
+}
