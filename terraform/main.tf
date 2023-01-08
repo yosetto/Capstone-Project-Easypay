d7391db24738d4bc9c169a830d122d1bb31bd8e1
Youssef <ettolbayos@gmail.com>
2023-01-08 19:51:19 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,25 @@
 provider "aws" {
-   access_key = var.aws_access_key
-   secret_key = var.aws_secret_key
-   #token = var.aws_api_token
-   region = var.aws_region
-   #skip_credentials_validation = true
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  #token = var.aws_api_token
+  region = var.aws_region
+  #skip_credentials_validation = true
 }
 
 # TODO: hardcode all variables of the module directly in this file (except
 # private_key_file and public_key_file which are exposed to the user).
-module "network" {
-  source     = "weibeld/kubeadm/aws//modules/network"
-  version    = "~> 0.2"
-  cidr_block = "10.0.0.0/16"
-  tags       = { "terraform-kubeadm:cluster" = module.cluster.cluster_name }
-}
-
 module "cluster" {
-  source    = "weibeld/kubeadm/aws"
-  version   = "~> 0.2"
-  vpc_id    = module.network.vpc_id
-  subnet_id = module.network.subnet_id
-    # Required variables
+  source  = "weibeld/kubeadm/aws"
+  version = "0.2.5"
+
+  #master_instance_type = "t2.micro"
+  #worker_instance_type = "t2.micro"
+  # Required variables
   private_key_file = var.private_key
   public_key_file  = var.public_key
+
+  # Optional variables
+  cluster_name           = "single-cluster"
+  pod_network_cidr_block = var.pod_network_cidr
+  num_workers            = var.num_workers
 }
